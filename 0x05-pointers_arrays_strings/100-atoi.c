7f348d782ae3190045938dcc5d1491e6c59b3042
Tinsae <tinsget2@gmail.com>
2024-06-13 00:36:28 +0300

    Adding 100-atoi.c excercise

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,23 +15,23 @@ int _atoi(char *s)
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] == '-')
+		if (*(i+s) == '-')
 		{
 			sign = sign * -1;
 		}
-		if (s[i] == '+')
+		if (*(i+s) == '+')
 		{
 			sign = sign * 1;
 		}
-		if (s[i] >= '0' && s[i] <= '9')
+		if (*(i+s) >= '0' && *(i+s) <= '9')
 		{
-			int d = s[i] - '0';
+			int d = *(i+s) - '0';
 
 			digit = digit * pow;
 			digit = digit + d;
 			pow = 10;
 		}
-		else if (pow == 10 && !(s[i] >= '0' && s[i] <= '9'))
+		else if (pow == 10 && !(*(i+s) >= '0' && *(i+s) <= '9'))
 		{
 			return (digit * sign);
 		}
