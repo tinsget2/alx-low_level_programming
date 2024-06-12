f770933d7eb22a25365a60c17540b7bbe9c18990
Tinsae <tinsget2@gmail.com>
2024-06-13 00:29:51 +0300

    Adding 100-atoi.c excercise

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * * _atoi - convert a string to an integer.
  * *
@@ -9,45 +8,41 @@
 int _atoi(char *s)
 {
 	int i;
-    int sign = 1;
-    int digit = 0;
-    int pow =0;
+	int sign = 1;
+	int digit = 0;
+	int pow = 0;
 
 	i = 0;
 	while (s[i])
 	{
-		if(s[i] == '-')
-        {
-            sign = sign * -1;
-        }
+		if (s[i] == '-')
+		{
+			sign = sign * -1;
+		}
+		if (s[i] == '+')
+		{
+			sign = sign * 1;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			int d = s[i] - '0';
 
-        if(s[i] == '+')
-        {
-            sign = sign * 1;
-        }
-
-        if(s[i] >= '0' && s[i] <= '9')
-        {
-            int d = s[i] - '0';              
-            digit = digit * pow;
-            digit = digit + d;
-            pow = 10;   
-                  
-        }
-        else if(pow == 10 && !(s[i] >= '0' && s[i] <= '9'))
-        {
-            return digit * sign;
-        }
-        
-        i++;
+			digit = digit * pow;
+			digit = digit + d;
+			pow = 10;
+		}
+		else if (pow == 10 && !(s[i] >= '0' && s[i] <= '9'))
+		{
+			return (digit * sign);
+		}
+		i++;
 	}
-
-    if(digit == 0)
-    {
-        return 0;
-    }
-    else
-    {
-        return digit * sign;
-    }
+	if (digit == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (digit * sign);
+	}
 }
